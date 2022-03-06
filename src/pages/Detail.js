41502663325e21a823b280fb084f112298ebc32c
Jorge <jorgeroca1996@gmail.com>
2022-03-06 21:02:51 +0100

    Pagina de Detalle de Gif

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Gif from "components/Gif/Gif";
-import useGlobalGifs from "hooks/useGlobalGifs";
+import useSingleGif from "hooks/useSingleGif";
+import Loader from "components/Loader/Loader";
+import { Redirect } from "wouter";
 
 export default function Detail({ params }) {
-  const gifs = useGlobalGifs();
+  const { gif, isLoading, isError } = useSingleGif({ id: params.id });
 
-  const gif = gifs.find((singleGif) => singleGif.id === params.id);
+  if (isLoading) return <Loader />;
+  if (isError) return <Redirect to="404" />;
+  if (!gif) return null;
 
-  return <Gif {...gif} />;
+  return (
+    <>
+      <h1 className="App-title">{gif.title}</h1>
+      <Gif {...gif} />
+    </>
+  );
 }
