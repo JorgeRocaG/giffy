292ee028c94d9c2de4a8b687909c67116cf56b09
Jorge <jorgeroca1996@gmail.com>
2022-03-12 19:16:33 +0100

    useReducer de SearchForm en un hook

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,40 +1,17 @@
-import React, { useState, useReducer } from "react";
+import React from "react";
 import { useLocation } from "wouter";
 
+import useForm from "./hooks";
+
 const RATINGS = ["g", "pg", "pg-13", "r"];
-
-const ACTIONS = {
-  UPDATE_KEYWORD: "update_keyword",
-  UPDATE_RATING: "update_rating",
-};
-
-const ACTION_REDUCERS = {
-  [ACTIONS.UPDATE_KEYWORD]: (state, action) => ({
-    ...state,
-    keyword: action.payload,
-    times: state.times + 1,
-  }),
-  [ACTIONS.UPDATE_RATING]: (state, action) => ({
-    ...state,
-    rating: action.payload,
-  }),
-};
-
-const reducer = (state, action) => {
-  const actionReducer = ACTION_REDUCERS[action.type];
-  return actionReducer ? actionReducer(state, action) : state;
-};
 
 function SearchForm({ initialKeyword = "", initialRating = RATINGS[0] }) {
   const [, pushLocation] = useLocation();
 
-  const [state, dispatch] = useReducer(reducer, {
-    keyword: decodeURIComponent(initialKeyword),
-    rating: initialRating,
-    times: 0,
+  const { keyword, times, rating, updateKeyword, updateRating } = useForm({
+    initialKeyword,
+    initialRating,
   });
-
-  const { keyword, times, rating } = state;
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -42,11 +19,11 @@ function SearchForm({ initialKeyword = "", initialRating = RATINGS[0] }) {
   };
 
   const handleChange = (e) => {
-    dispatch({ type: ACTIONS.UPDATE_KEYWORD, payload: e.target.value });
+    updateKeyword(e.target.value);
   };
 
   const handleChangeRating = (e) => {
-    dispatch({ type: ACTIONS.UPDATE_RATING, payload: e.target.value });
+    updateRating(e.target.value);
   };
 
   return (
